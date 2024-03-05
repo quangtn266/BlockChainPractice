595f20185b323c7b00ed043a5fe4a0eaa70de6c7
quangtn266 <quangtrandn93@gmail.com>
2024-03-05 15:45:33 +0700

    update source code for address and account balance

--- src/model/address.rs
+++ src/model/address.rs
@@ -3,6 +3,8 @@ use std::{
     fmt,
     str::FromStr,
 };
+use std::fmt::Formatter;
+use actix_web::body::MessageBody;
 
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
@@ -37,4 +39,119 @@ impl TryFrom<Vec<Byte>> for Address {
     }
 }
 
-impl
+impl TryFrom<String> for Address {
+    type Error = AddressError;
+
+    fn try_from(s: String) -> Result<Self, AddressError> {
+        match hex::decode(s) {
+            Ok(decoded_vec) => decoded_vec.try_into(),
+            Err(_) => Err(AddressError::InvalidFormat),
+        }
+    }
+}
+
+impl FromStr for Address {
+    type Err =AddressError;
+
+    fn from_str(s: &str) -> Result<Self, AddressError> {
+        Address::try_from(s.to_string())
+    }
+}
+
+impl From<Address> for String {
+    fn from(account: Address) -> Self {
+        account.to_string()
+    }
+}
+
+impl fmt::Display for Address {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", hex::encode(self.0))
+    }
+}
+
+// Some sample addresses to be used in tests all over the project
+// we export functions to workaround constanst value restrictions in Rust
+#[cfg(test)]
+pub mod test_util {
+    use std::convert::TryFrom;
+    use std::ops::Add;
+
+    use super::Address;
+
+    pub fn alice() -> Address {
+        Address::try_from(
+            "f780b958227ff0bf5795ede8f9f7eaac67e7e06666b043a400026cbd421ce28e".to_string(),
+        ).unwrap()
+    }
+
+    pub fn bob() -> Address {
+        Address::try_from(
+            "51df097c03c0a6e64e54a6fce90cb6968adebd85955917ed438e3d3c05f2f00f".to_string(),
+        ).unwrap()
+    }
+
+    pub fn carol() -> Address {
+        Address::try_from(
+            "b4f8293fb123ef3ff9ad49e923f4afc732774ee2bfdc3b278a359b54473c2277".to_string(),
+        ).unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{convert::TryFrom, str::FromStr};
+
+    use crate::model::Address;
+
+    use super::AddressError;
+
+    #[test]
+    fn parse_valid_address() {
+        let hex_str = "f780b958227ff0bf5795ede8f9f7eaac67e7e06666b043a400026cbd421ce28e";
+        let address = Address::try_from(hex_str.to_string()).unwrap();
+        assert_eq!(address.to_string(), hex_str);
+        let address = Address::from_str(hex_str).unwrap();
+        assert_eq!(address.to_string(), hex_str);
+    }
+
+    #[test]
+    fn parse_case_insensitive() {
+        let hext_str = "F780B958227ff0bf5795ede8f9f7eaac67e7e06666b043a400026cbd421ce28e".to_string();
+        let address = Address::try_from(hext_str.clone()).unwrap();
+        assert_eq!(address.to_string(), hext_str.to_lowercase());
+    }
+
+    #[test]
+    fn parse_json() {
+        let hex_str = "f780b958227ff0bf5795ede8f9f7eaac67e7e06666b043a400026cbd421ce28e".to_string();
+        let address: Address = serde_json::from_value(serde_json::Value::String(hex_str.clone())).unwrap();
+        assert_eq!(address.to_string(), hex_str.to_lowercase());
+        let address_json = serde_json::to_value(address).unwrap();
+        assert_eq!(address_json, serde_json::Value::String(hex_str.clone()));
+
+        #[test]
+        fn reject_too_short() {
+            // 31-byte string (62 hex chars)
+            let hex_str = "f780b958227ff0bf5795ede8f9f7eaac67e7e06666b043a400026cbd421ce2".to_string();
+            let err = Address::try_from(hex_str).unwrap();
+            assert_eq!(err, AddressError::InvalidLength);
+        }
+
+        #[test]
+        fn reject_too_long() {
+            // 33-byte string (66 hex chars)
+            let hex_str = "f780b958227ff0bf5795ede8f9f7eaac67e7e06666b043a400026cbd421ce28e10".to_string();
+            let err =Address::try_from(hex_str).unwrap_err();
+            assert_eq!(err, AddressError::InvalidLength);
+        }
+
+        #[test]
+        fn reject_invalid_characters() {
+            // correct length (32 bytes) but with an invalid hexadecimal char "g"
+            let hex_str = "g780b958227ff0bf5795ede8f9f7eaac67e7e06666b043a400026cbd421ce28e".to_string();
+            let err = Address::try_from(hex_str).unwrap_err();
+            assert_eq!(err, AddressError::InvalidFormat);
+        }
+    }
+}
